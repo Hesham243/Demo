944c3297e032b6b732af10b0b5928c66c1bc1b7b
Hesham243 <heshamahmed243@gmail.com>
2023-12-13 14:41:58 +0300

    Update Client1.py

--- Client1.py
+++ Client1.py
@@ -1,10 +1,8 @@
 import socket
 socket_c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_c.bind(("127.0.0.1",4466))
-socket_c.listen(2)
-sock_a, addr = socket_c.accept()
+socket_c.connect(("127.0.0.1",4466))
 
-msg = sock_a.recv()
+msg = socket_c.recv()
 print(msg.decode())
 
-sock_a.close()
+socket_c.close()
